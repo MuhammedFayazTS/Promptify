46234ebbaa9b6a37b601b047c54a9380714a6d37
Muhammed Fayaz T S <muhammedfayazts01@gmail.com>
2024-03-25 16:56:15 +0530

    add update and delete prompts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,12 @@ import { useState } from "react"
 
 
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleelete }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
+
   const [copied, setCopied] = useState("")
 
   const handleCopy = () => {
@@ -43,7 +48,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleelete }) => {
             src={copied === post.prompt
               ? '/assets/icons/tick.svg'
               : '/assets/icons/copy.svg'}
-              alt="copy_image"
+            alt="copy_image"
             width={12}
             height={12}
           />
@@ -53,8 +58,26 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleelete }) => {
       <p className="my-4 font-sathoshi text-sm text-gray-700">{post.prompt}</p>
       <p className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}>
-        {post.tag}
+       #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' &&
+        (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
 
     </div>
   )
